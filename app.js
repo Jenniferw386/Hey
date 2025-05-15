11ed7c378f4e84a8a58bd74b3137d8aea16214d2
Jennifer <jenniferw386@gmail.com>
2025-05-15 01:20:29 -0400

    added drawings

--- app.js
+++ app.js
@@ -2,6 +2,26 @@
 //service_5qg5ngh
 //4k8_T225bAJgNZ9FV
 
+const scaleFactor = 1 / 20
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i =0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+
+
+
+
+
+
+
 let contrastToggle = false;
 
 function toggleContrast() {
